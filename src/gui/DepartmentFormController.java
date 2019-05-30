a16f0918f2d9f5f0c3662bef6e94ccb00f71d67a
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-05-29 21:16:27 -0300

    Salvando um novo departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,20 +1,29 @@
 package gui;
 
 import java.net.URL;
+import java.nio.channels.IllegalSelectorException;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
-	
+
+	private DepartmentService service;
+
 	private Department entity;
-	
+
 	@FXML
 	private TextField txtId;
 
@@ -31,13 +40,36 @@ public class DepartmentFormController implements Initializable {
 	private Button btCancel;
 
 	@FXML
-	public void onbtSaveAction() {
-		System.out.println("onbtSaveAction");
+	public void onbtSaveAction(ActionEvent event) {
+
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+
+		} catch (DbException e) {
+
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+
+		return obj;
 	}
 
 	@FXML
-	public void onbtCancelAction() {
-		System.out.println("onbtCancelAction");
+	public void onbtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	@Override
@@ -47,20 +79,23 @@ public class DepartmentFormController implements Initializable {
 
 	public void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);		
+		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
+
 	public void updateFormData() {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(this.entity.getId()));
 		txtName.setText(this.entity.getName());
-
 	}
 
 }
